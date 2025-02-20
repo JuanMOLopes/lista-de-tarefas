4e02319f0f397c992b5607df45fb6da114834903
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-02-20 09:36:50 -0300

    estrutura finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import ListaTarefas from "./components/ListaDeTarefas/ListaTarefas";
 
 function App() {
   return (
     <>
-      
+      <ListaTarefas />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
