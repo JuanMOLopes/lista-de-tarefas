59f15f48a1745a4bb4aef949088a1fe66a28144e
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-02-20 10:18:40 -0300

    css tarefas arrumado

--- src/components/Tarefas/Tarefa.jsx
+++ src/components/Tarefas/Tarefa.jsx
@@ -3,28 +3,31 @@ import "./Tarefa.css"; /* Importa o css do componente */
 
 function Concluir(props) {
   // Estado status (verdadeiro ou falso), iniciando como falso
-  const [status, setStatus] = useState(false); 
+  const [status, setStatus] = useState(false);
   // Estado cor iniciado como "Concluir"
-  const [cor, setCor] = useState("Concluir"); 
+  const [cor, setCor] = useState("Concluir");
 
   /* Função "Seguir" que é chamada ao clicar no botão */
   function AlternarStatus() {
     // Inverte o valor do status (se estava verdadeiro, inverte para falso, e vice-versa)
-    setStatus(!status); 
+    setStatus(!status);
     // Se o status estiver como verdadeiro, define "cor" como "Concluir", se for falso, como "Concluido"
-    setCor(status ? "Concluir" : "Concluido"); 
+    setCor(status ? "Concluir" : "Concluido");
   }
 
   return (
     // Define a classe da div pelo estado "cor"
-    <div>
-<p className={status ? "Concluido" : "Concluir"}>{props.titulo}</p>
+    <div className="tarefa">
+      <p className={status ? "Concluido" : "Concluir"}>{props.titulo}</p>
       {/* Ao clicar no botão, chama a função "AlternarStatus", a classe é definida pela lógica "Se o status estiver como verdadeiro, define "cor" como "Concluir", se for falso, como "Concluido", o texto de dentro do botão também é definido pela mesma lógica */}
-      <button onClick={AlternarStatus} className={status ? "Concluido" : "Concluir"}>
+      <button
+        onClick={AlternarStatus}
+        className={status ? "Concluido" : "Concluir"}
+      >
         {status ? "Concluido" : "Concluir"}
       </button>
     </div>
-    //teste 
+    //teste
   );
 }
 
